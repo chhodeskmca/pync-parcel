6ab35bd39bec3f7aa70d8945bb1ed83b13ab8153
Rahul Chhabra <chhabra.odesk.mca@gmail.com>
2025-09-17 12:07:17 +0530

    `Fixed whitespace and formatting inconsistencies in updateprealert.php`

--- user-dashboard/updateprealert.php
+++ user-dashboard/updateprealert.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
     // initialize session
-    session_start();  
-	include('../config.php'); // database connection
-	include('../function.php'); // function
-    include('../user-area/authorized-user.php'); // function
-	$current_file_name =  basename($_SERVER['PHP_SELF']);  // getting current file name 
+    session_start();
+    include '../config.php';                             // database connection
+    include '../function.php';                           // function
+    include '../user-area/authorized-user.php';          // function
+    $current_file_name = basename($_SERVER['PHP_SELF']); // getting current file name
 ?>
 
 <!DOCTYPE html>
@@ -17,7 +17,7 @@
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.13.1/css/alertify.min.css" integrity="sha512-IXuoq1aFd2wXs4NqGskwX2Vb+I8UJ+tGJEu/Dc0zwLNKeQ7CW3Sr6v0yU3z5OQWe3eScVIkER4J9L7byrgR/fA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	  <!-- CSS for Tracking icons -->
 	 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-     <link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/tutorials/timelines/timeline-5/assets/css/timeline-5.css"> 
+     <link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/tutorials/timelines/timeline-5/assets/css/timeline-5.css">
     <!-- CSS Files -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/css/kaiadmin.min.css" />
@@ -58,7 +58,7 @@
               <li class="nav-item active">
                 <a href="index.php">
                    <img class="home-icon" src="assets/img/home.png" alt="home" />
-                  <p>Dashboard</p> 
+                  <p>Dashboard</p>
                 </a>
               </li>
               <li class="nav-item">
@@ -78,7 +78,7 @@
                 <img class="calculator-icon" src="assets/img/calculator.png" alt="Calculator" />
                   <p>Cost Calculator</p>
                 </a>
-              </li> 
+              </li>
               <li class="nav-item">
                 <a href="makepayment.php">
                   <img class="payment-icon" src="assets/img/payment-protection.png" alt="payment" />
@@ -167,7 +167,7 @@
                     <li>
                       <div class="notif-scroll scrollbar-outer">
                         <div class="notif-center">
-						  <div class="notifi-area"> 
+						  <div class="notifi-area">
                           <a href="#">
                             <div style="background:#E87946" class="notif-icon">
                                <img width="30px" height="30px" src="assets/img/delivery.png" alt="delivery" />
@@ -181,7 +181,7 @@
 						    <img src="assets/img/close.png" alt="close" />
 						  </span>
                           </div>
-						  <div class="notifi-area"> 
+						  <div class="notifi-area">
 						  <a href="#">
                             <div style="background:#000" class="notif-icon">
                                  <img width="30px" height="30px" src="assets/img/shipped.png" alt="shipped" />
@@ -197,7 +197,7 @@
 						     <img src="assets/img/close.png" alt="close" />
 						  </span>
 						  </div>
-						  <div class="notifi-area"> 
+						  <div class="notifi-area">
                           <a href="#">
                             <div style="background:#226424" class="notif-img">
                               <img  style="width: 30px !important; height: 30px !important"
@@ -216,7 +216,7 @@
 						     <img src="assets/img/close.png" alt="close" />
 						   </span>
 						   </div>
-						   <div class="notifi-area"> 
+						   <div class="notifi-area">
                           <a href="#">
                             <div class="notif-icon notif-danger">
                                <img  style="width: 30px !important; height: 30px !important"
@@ -233,7 +233,7 @@
 						     <img src="assets/img/close.png" alt="close" />
 						   </span>
 						   </div>
-						  <div class="notifi-area"> 
+						  <div class="notifi-area">
 							  <a href="#">
 								<div class="notif-icon notif-danger">
 									<img  style="width: 30px !important; height: 30px !important"
@@ -257,7 +257,7 @@
                 </li>
                 <li class="nav-item">
                       <span class="op-7">Welcome</span>
-                      <span class="fw-bold"><?php echo user_account_information()['first_name'] ; ?></span> 
+                      <span class="fw-bold"><?php echo user_account_information()['first_name']; ?></span>
                 </li>
               </ul>
             </div>
@@ -266,39 +266,37 @@
         </div>
         <div class="container">
           <div class="page-inner">
-            <!--update Prealert  start--> 
+            <!--update Prealert  start-->
 			 <div class="row">
               <div class="col-md-12">
-                <div class="card"> 
-				<?php 
-				
-				
-				
-				 if( isset($_REQUEST['pre_alert_id']) && $_REQUEST['pre_alert_id'] != "" ){ 
-				  
-				     $Pre_alert_id = $_REQUEST['pre_alert_id'];
-					 
-     $sql = "SELECT * FROM pre_alert where id = $Pre_alert_id AND User_id = $user_id";
+                <div class="card">
+				<?php
 
-				    if( mysqli_num_rows( mysqli_query($conn, $sql)) == 1  ){
+                    if (isset($_REQUEST['pre_alert_id']) && $_REQUEST['pre_alert_id'] != "") {
 
-					   $rows =  mysqli_fetch_array(mysqli_query($conn, $sql));
+                        $Pre_alert_id = $_REQUEST['pre_alert_id'];
 
-					   // Check if editing is locked after 24 hours
-					   if (!isset($rows['created_at']) || empty($rows['created_at'])) {
-					       // Old records without created_at are considered old, lock them
-					       echo "<h2 style='text-align: center; padding: 50px; font-size: 20px;line-height: 21px;'>This pre-alert cannot be edited as it was created more than 24 hours ago.</h2>";
-					       exit;
-					   } else {
-					       $created_at = strtotime($rows['created_at']);
-					       $now = time();
-					       if (($now - $created_at) > 86400 || $created_at > $now) { // lock if more than 24 hours old or future date
-					           echo "<h2 style='text-align: center; padding: 50px; font-size: 20px;line-height: 21px;'>This pre-alert cannot be edited as it was created more than 24 hours ago.</h2>";
-					           exit;
-					       }
-					   }
-					
-					?>
+                        $sql = "SELECT * FROM pre_alert where id = $Pre_alert_id AND User_id = $user_id";
+
+                        if (mysqli_num_rows(mysqli_query($conn, $sql)) == 1) {
+
+                            $rows = mysqli_fetch_array(mysqli_query($conn, $sql));
+
+                            // Check if editing is locked after 24 hours
+                            if (! isset($rows['created_at']) || empty($rows['created_at'])) {
+                                // Old records without created_at are considered old, lock them
+                                echo "<h2 style='text-align: center; padding: 50px; font-size: 20px;line-height: 21px;'>This pre-alert cannot be edited as it was created more than 24 hours ago.</h2>";
+                                exit;
+                            } else {
+                                $created_at = strtotime($rows['created_at']);
+                                $now        = time();
+                                if (($now - $created_at) > 86400 || $created_at > $now) { // lock if more than 24 hours old or future date
+                                    echo "<h2 style='text-align: center; padding: 50px; font-size: 20px;line-height: 21px;'>This pre-alert cannot be edited as it was created more than 24 hours ago.</h2>";
+                                    exit;
+                                }
+                            }
+
+                        ?>
 				<form class="Pre_alert_updating" action="../codes.php" method="POST" enctype="multipart/form-data">
 				   <input type="text" name="Pre_alert_id" value="<?php echo $rows['id']; ?>" hidden />
                   <div class="card-header">
@@ -326,18 +324,18 @@
                             id="value"
                             placeholder="Enter value"
                           />
-                        </div>					
+                        </div>
                       </div>
                       <div class="col-md-6 col-lg-4">
 					   <div class="form-group">
                           <label for="exampleFormControlSelect1">Courier Company</label>
                           <select name="courier_company" class="form-select" id="exampleFormControlSelect1">
-                            <option <?php echo $rows['courier_company'] == 'Amazon' ? 'selected' : ''  ; ?> >Amazon</option>
-                            <option <?php echo $rows['courier_company'] == 'DHL' ? 'selected' : ''  ; ?> >DHL</option> 
-                            <option <?php echo $rows['courier_company'] == 'FedEx' ? 'selected' : ''  ; ?> >FedEx</option>
-                            <option <?php echo $rows['courier_company'] == 'UPS'? 'selected' : ''  ; ?>>UPS</option>
-                            <option <?php echo $rows['courier_company'] == 'USPS'? 'selected' : '' ; ?>>USPS</option>
-                            <option <?php echo $rows['courier_company'] == 'Other'? 'selected' : ''  ; ?> >Other</option>
+                            <option                                    <?php echo $rows['courier_company'] == 'Amazon' ? 'selected' : ''; ?> >Amazon</option>
+                            <option                                    <?php echo $rows['courier_company'] == 'DHL' ? 'selected' : ''; ?> >DHL</option>
+                            <option                                    <?php echo $rows['courier_company'] == 'FedEx' ? 'selected' : ''; ?> >FedEx</option>
+                            <option                                    <?php echo $rows['courier_company'] == 'UPS' ? 'selected' : ''; ?>>UPS</option>
+                            <option                                    <?php echo $rows['courier_company'] == 'USPS' ? 'selected' : ''; ?>>USPS</option>
+                            <option                                    <?php echo $rows['courier_company'] == 'Other' ? 'selected' : ''; ?> >Other</option>
                           </select>
                         </div>
 						 <div class="form-group">
@@ -359,79 +357,79 @@
 							  <label for="id">invoice
 							  </label>
 							  <br />
-							  <input  
-								hidden  
-								name='old_image'  
+							  <input
+								hidden
+								name='old_image'
 								value='<?php echo $rows['invoice']; ?>'
-								type="text"  
+								type="text"
 							  />
-								
-							  <input  
-							   name='file'  
-							   type="file" 
-							   class="form-control-file w-100"  
-							   id="id" 
+
+							  <input
+							   name='file'
+							   type="file"
+							   class="form-control-file w-100"
+							   id="id"
 							   style="
-							   border: 2px solid #E87946; color:<?php echo $rows['invoice'] == "" ? 'black' : '#fff0'  ; ?>"
+							   border: 2px solid #E87946; color:<?php echo $rows['invoice'] == "" ? 'black' : '#fff0'; ?>"
 							  />
 							   <br />
 							   <p style='margin-bottom:0px' class="fileAllowed"><b>Allowed formats : </b>PDF, DOC, DOCX, JPEG or PNG (MAX. 10MB)</p>
-									<p style='margin-bottom: 0px; margin-top: 5px; color: #bf1919; font-size: 16px;  line-height: 17px;'> 
+									<p style='margin-bottom: 0px; margin-top: 5px; color: #bf1919; font-size: 16px;  line-height: 17px;'>
 								</p>
-								<div style="margin-bottom: 10px; width: 250px; display: flex;  border: 1px solid #ddd;  padding: 9px;  border-radius: 6px;  color: #201f1f;  font-size: 14px;  position: relative;"> 
-								  <?php 
-										if(  ltrim($rows['invoice']) != '' ){  
-								   ?> 
-										<span style=" display: -webkit-box;  -webkit-line-clamp: 1;  -webkit-box-orient: vertical;  overflow: hidden;  width: 180px;">  
-										
-										  <?php echo $rows['invoice'] ; ?> 
-										  
+								<div style="margin-bottom: 10px; width: 250px; display: flex;  border: 1px solid #ddd;  padding: 9px;  border-radius: 6px;  color: #201f1f;  font-size: 14px;  position: relative;">
+								  <?php
+                                      if (ltrim($rows['invoice']) != '') {
+                                              ?>
+										<span style=" display: -webkit-box;  -webkit-line-clamp: 1;  -webkit-box-orient: vertical;  overflow: hidden;  width: 180px;">
+
+										  <?php echo $rows['invoice']; ?>
+
 										  </span>
 										<a style="width: 18px; position: absolute;  right: 10px;  top: 11px; "
-										 class="d-flex" 
-										 href="../uploaded-file/<?php echo $rows['invoice'] ; ?>">
-										   <img 
-											  width='18px;' 
-											  src="assets/img/hide.png"  
-											  alt="" 
+										 class="d-flex"
+										 href="../uploaded-file/<?php echo $rows['invoice']; ?>">
+										   <img
+											  width='18px;'
+											  src="assets/img/hide.png"
+											  alt=""
 											/>
 										</a>
-									<?php }else{ 
-									  
-									   echo "No invoice attached";
-									
-									 } ?>
+									<?php } else {
+
+                                                    echo "No invoice attached";
+
+                                            }?>
 								</div>
-                           </div> 
+                           </div>
                       </div>
 					</div>
                   </div>
                   <div class="card-action d-flex">
 				    <input  hidden type="text" name="updatePreAltBtn" />
-                     <button name="updatePreAltBtn" class="btn updatePreAltBtn"> 
+                     <button name="updatePreAltBtn" class="btn updatePreAltBtn">
 					 <img style='display:none' class='spinner'  width="20px" src="../assets/img/spinner.gif" alt="" />
-					   Update 
+					   Update
 					 </button>
                   </div>
 				  </form>
-				<?php  
-				
-				    }else{ 
-					 echo "
+				<?php
+
+                        } else {
+                            echo "
 					        <h2 style='text-align: center; padding: 50px;  font-size: 20px;line-height: 21px;'>
 					         Something went wrong.
-					        </h2> 
+					        </h2>
 					      ";
-					};
-				  }else{ 
-				  
-				   echo "
+                        }
+                    } else {
+
+                        echo "
 					    <h2 style='text-align: center; padding: 50px;  font-size: 20px;line-height: 21px;'>
 					      Something went wrong.
-					    </h2> 
+					    </h2>
 					 ";
-				  }				  
-				?> 
+                    }
+                ?>
                 </div>
               </div>
             </div>
@@ -444,7 +442,7 @@
           </div>
         </footer>
       </div>
-    </div> 
+    </div>
     <!--   boostrap   -->
     <script src="assets/js/core/jquery-3.7.1.min.js"></script>
     <script src="assets/js/core/popper.min.js"></script>
@@ -455,11 +453,11 @@
     <script src="assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
     <!-- Kaiadmin JS -->
       <script src="assets/js/kaiadmin.min.js"></script>
-	  
-	  <script type="text/javascript"> 
-	  
+
+	  <script type="text/javascript">
+
 	    // Submission form will start after 1 second
-	    $(document).ready(function() {  
+	    $(document).ready(function() {
 		  $('.Pre_alert_updating').on('submit', function(e) {
 			 $('.spinner').css('display', 'inline');
 			e.preventDefault(); // Stop form from submitting immediately
@@ -468,23 +466,23 @@
 			  form.submit(); // Native submit after delay
 			}, 1000); // 5000ms = 5 seconds
 		  });
-		}); 
-	  
+		});
+
 	  </script>
 		<?php
-		   if( isset($_SESSION['message']) ){  
-		?>  
+            if (isset($_SESSION['message'])) {
+            ?>
 		<script>
 			// ===== alertify======
 			alertify.set('notifier','position', 'top-right');
-			alertify.success("<?php echo $_SESSION['message'] ; ?>");
-		</script>  
-		 <?php  
-			unset($_SESSION['message']);
-			};
-		 ?> 
-		 
+			alertify.success("<?php echo $_SESSION['message']; ?>");
+		</script>
+		 <?php
+             unset($_SESSION['message']);
+             }
+         ?>
+
 		<!-- custom js -->
-		<script src="assets/js/custom.js"></script>  
+		<script src="assets/js/custom.js"></script>
   </body>
 </html>
